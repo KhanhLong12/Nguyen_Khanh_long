e27dbfc9df072885e5e29cd40fb8216c7c2f4e78
KhanhLong12 <basturlong@gmail.com>
2019-05-17 17:35:59 +0700

    khanhlong

--- BTVN/bt12/bai1/js/main.js
+++ BTVN/bt12/bai1/js/main.js
@@ -1,15 +1,16 @@
 $('.anhcon').on('click',function(){
 	var id = $(this).data('id');
 		$('#anh').fadeOut(0);
-		$('#anh').siblings('h2').fadeOut(0);
+		$('#anh').siblings('h1').fadeOut(0);
 		$('.close').css('display','block');
 		$('.anhtocon').css('display','none')
 		$('#anhtocon'+id).fadeIn(500);
+		$('#mota'+id).slideDown(800);
 	})
 $('.close').on('click', function(){
 	$('.close').css('display','none');
 	$('#anh').show(500);
-	$('#anh').siblings('h2').show(500);
+	$('#anh').siblings('h1').show(500);
 	$('.anhtocon').slideUp(500);
+	$('.mota').slideUp(500);
 })
-
